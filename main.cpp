d54e4d93db02649fe2cd611c4fdc7a9e8aa722a0
Zachary <zalfakir@yahoo.com>
2014-02-12 15:29:40 -0500

    Changed version number

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ robot_class::robot_class()
 
 void robot_class::RobotInit()
 {
-    std::printf("TEST CODE 1.0");
+    std::printf("TEST CODE 1.1");
     robot = this;
     driverJoy = new SmoothJoystick(DRIVER_JOY_PORT);
     
